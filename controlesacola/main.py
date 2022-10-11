bc82fa8a5dcdab33213ee868a0be31add6bb8d73
João Batista <batistajoao54.ba@gmail.com>
2022-10-10 21:35:16 -0300

    Update main.py

--- controlesacola/main.py
+++ controlesacola/main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import streamlit as st
 import plotly.express as px
+import openpyxl
 
 st.set_page_config(layout = 'wide')
 
@@ -10,6 +11,8 @@ pagina = st.sidebar.checkbox('dados carregados')
 
 if pagina == True:
     df = pd.read_csv(dados)
+    df['QUANTIDADE'] = pd.to_numeric(df["QUANTIDADE"])
+    
     lista_Os = df.OS.unique()
 
     #SELECIONADO A OS
@@ -17,22 +20,24 @@ if pagina == True:
     df_os = df[df["OS"] == cx_os]
 
     df_total = df_os[df_os['STATUS'] == "TOTAL OS"]
-
+            
     #SELECIONANDO O TOTAL DE SACOLAS ENVIADAS e recebidas POR OS
     df_base = df[df['OS'] == cx_os]
+    df_base['QUANTIDADE'] = pd.to_numeric(df_base['QUANTIDADE'])
     df_recebido = df_base[df_base["STATUS"] == 'RECEBIDO']
     df_enviado = df_base[df_base['STATUS'] == 'ENVIADO']
-
+    
     soma_enviado = df_enviado.copy()
-
+    
     soma_recebido = df_recebido.copy()
+    
 
-    juncao = soma_enviado.groupby(["OS", "TAMANHO", "STATUS"]).sum().reset_index()
-    juncao2 = soma_recebido.groupby(["OS", "TAMANHO", "STATUS"]).sum().reset_index()
+    juncao = soma_enviado.groupby(["OS", "TAMANHO", "STATUS",]).sum().reset_index()
+    juncao2 = soma_recebido.groupby(["OS", "TAMANHO", "STATUS",]).sum().reset_index()
 
     df_total_final = pd.concat([df_total,juncao,juncao2])
-
-    c1,c2 = st.columns([1,1])
+   
+    c1,c,c2 = st.columns([1,0.2,1])
 
     with c1:
         fig_inicial = px.bar(df_total_final, x="OS", y="QUANTIDADE", color='STATUS',
@@ -45,34 +50,40 @@ if pagina == True:
 
     with c2:
         df_dropidez = df_os[df_os['STATUS'] != "TOTAL OS"].copy()
-        coladores = df_dropidez.groupby(["COLADOR", "MARCA", "TAMANHO", "COR", "OS", "STATUS",'DIA','MES','ANO']).sum().reset_index()
+        df_dropidez['QUANTIDADE'] = pd.to_numeric(df_dropidez['QUANTIDADE'])
+        coladores = df_dropidez.groupby(["COLADOR", "MARCA", "TAMANHO", "COR", "OS", "STATUS",]).sum().reset_index()
         coladores_enviado = coladores[coladores["STATUS"] == "ENVIADO"]
         coladores_recebido = coladores[coladores["STATUS"] == "RECEBIDO"]
-
+        
         novo_coladores = pd.concat([coladores_enviado, coladores_recebido])
 
-        fig_final = px.bar(novo_coladores, x="COLADOR", y="QUANTIDADE",
-                           color="STATUS", text_auto=True,
-                           barmode="group", height=400, width=600,
-                           title="COLADORES")
-        st.plotly_chart(fig_final)
-
-        #transformando os número em data
-        df_data = df_dropidez.copy()
-        df_data["DATA"] = f"{df_data.iloc[0,0]:.0f}-{df_data.iloc[0,1]:.0f}-{df_data.iloc[0,2]:.0f}"
-        df_data_atual = df_data[df_data['COLADOR'] != "IDEZ"]
-        lista_data = list(df_data_atual['COLADOR'].unique())
+        fig2 = st.checkbox("VER COLADORES")
         
-        registros = st.checkbox("Verificar tabelas")
-        
-        if registros == True:
-            pesquisa = st.selectbox("Colador", lista_data)
-
-            df_tabela = df_data_atual[['DATA',"COLADOR",'QUANTIDADE','STATUS']]
+        if fig2 == True:
             
-            df_tabela_final = df_tabela[df_tabela['COLADOR'] == pesquisa]
-            df_apresentacao = df_tabela_final[['DATA','QUANTIDADE','STATUS']]
-            st.table(df_apresentacao)
+            fig_final = px.bar(novo_coladores, x="COLADOR", y="QUANTIDADE",
+                               color="STATUS", text_auto=True,
+                               barmode="group", height=400, width=500,
+                               title="COLADORES")
+
+            st.plotly_chart(fig_final)
+
+            #transformando os número em data
+            df_data = df_dropidez.copy()
+            df_data["DATA"] = f"{df_data.iloc[0,0]:.0f}-{df_data.iloc[0,1]:.0f}-{df_data.iloc[0,2]:.0f}"
+            df_data_atual = df_data[df_data['COLADOR'] != "IDEZ"]
+            lista_data = list(df_data_atual['COLADOR'].unique())
+        
+            registros = st.checkbox("Verificar tabelas")
+        
+            if registros == True:
+                pesquisa = st.selectbox("Colador", lista_data)
+
+                df_tabela = df_data_atual[['DATA',"COLADOR",'QUANTIDADE','STATUS']]
+            
+                df_tabela_final = df_tabela[df_tabela['COLADOR'] == pesquisa]
+                df_apresentacao = df_tabela_final[['DATA','QUANTIDADE','STATUS']]
+                st.table(df_apresentacao)
         
         
     st.header("Dados gerais de todas as OS ativas")
