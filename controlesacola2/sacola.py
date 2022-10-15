a25240f7e3b9771d08228a0a14412da53ae61ab2
João Batista <batistajoao54.ba@gmail.com>
2022-10-15 17:08:20 -0300

    atualizacao

--- controlesacola2/sacola.py
+++ controlesacola2/sacola.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import plotly.express as px
 import streamlit as st
 import csv
 
@@ -70,7 +71,7 @@ if pagina == "ATUALIZAR":
         with coluna1:
             lista_colador = df_os['MARCA'].unique()
             marca_seletor = st.selectbox("MARCA", lista_colador)
-        df_marca = df[df['MARCA'] == marca_seletor]
+        df_marca = df_os[df_os['MARCA'] == marca_seletor]
         lista_os = df_marca['OS'].unique()
         with coluna2:
             os_seletor = st.selectbox("OS",lista_os)
@@ -129,10 +130,87 @@ if pagina == "ATUALIZAR":
         
         c12,c13,c14 = st.columns([1,2,2])
         df_visualizacao = pd.read_csv('entrada e saida.csv')
-        st.write("QUANTIDADE DE DADOS REGISTRADOS")
+        st.write("ULTIMOS REGISTRADOS")
         with c12:
             quantidade_dados = st.selectbox('',[3,5,4,2,1,10,15,20,30])
         st.table(df_visualizacao.tail(int(quantidade_dados)))
+
+#INICAIANDO A SEGUNDA PARTE DO SISTEMA, ANALISE DOS DADOS
+if pagina == 'ANALISAR':
+    df2 = pd.read_csv('dados_os.csv')
+    df3 = pd.read_csv('entrada e saida.csv')
+    df_completo = pd.concat([df2,df3])
+    df_completo.to_excel('dadosatuais.xlsx', index = False) #essa primeira parte serve pra juntar e salva os dados atuais
+    
+    #agora com os dados atualizados e salvo iremos chamalos
+    
+    df_atual = pd.read_excel('dadosatuais.xlsx')
+    df_atual['QUANTIDADE'] = pd.to_numeric(df_atual["QUANTIDADE"])
+    
+    lista_marca = df_atual.MARCA.unique() #separando todas as marcas
+    
+    cx_marca = st.sidebar.selectbox("Escolha uma MARCA!",lista_marca) #criando uma seleçao para as marcas
+    df_marca2 = df_atual[df_atual['MARCA'] == cx_marca] #criando um df so com as marcas selecionada
+    lista_Os = df_marca2.OS.unique() #criando uma lista com todas as os
+    cx_os = st.sidebar.selectbox("Selecione uma OS", lista_Os) #criando uma selecao para as os
+    df_os2 = df_atual[df_atual["OS"] == cx_os] #selecionado apeanas os dados com as os
+    df_total2 = df_os2[df_os2['STATUS'] == "TOTAL OS"] #criando os dados apenas com as os
+    
+    #fazendo a selecao do total de enviados e recebidos
+    df_base = df[df['OS'] == cx_os]
+    df_recebido = df_base[df_base["STATUS"] == 'RECEBIDO']
+    df_enviado = df_base[df_base['STATUS'] == 'ENVIADO']
+    soma_enviado = df_enviado.copy()
+    soma_recebido = df_recebido.copy()
+    
+    juncao = soma_enviado.groupby(["OS", "TAMANHO", "STATUS",]).sum().reset_index()
+    juncao2 = soma_recebido.groupby(["OS", "TAMANHO", "STATUS",]).sum().reset_index()
+
+    df_total_final = pd.concat([df_total2,juncao,juncao2]) #juntando todas as informaçoes que precisamos
+    
+    c1,c,c2 = st.columns([1,0.2,1]) #criando outras colunas que iremos usar
+    
+    #criando o primeiro grafico
+    with c1:
+        fig_inicial = px.bar(df_total_final, x="OS", y="QUANTIDADE", color='STATUS',
+                             barmode='group', height=400, width=400,
+                             text_auto=True,
+                             title=f"SACOLAS {df_total2.iloc[0,4]} {df_total2.iloc[0,5]} {df_total2.iloc[0,6]}")
+
+        st.plotly_chart(fig_inicial)
+    
+    with c2:
+        df_dropidez = df_os2[df_os2['STATUS'] != "TOTAL OS"].copy()
+        coladores = df_dropidez.groupby(["COLADOR", "MARCA", "TAMANHO", "COR", "OS", "STATUS",]).sum().reset_index()
+        coladores_enviado = coladores[coladores["STATUS"] == "ENVIADO"]
+        coladores_recebido = coladores[coladores["STATUS"] == "RECEBIDO"]
+
+        novo_coladores = pd.concat([coladores_enviado, coladores_recebido]) #juntando as informaç~es dos coladores
+
+        fig2 = st.checkbox("VER COLADORES")
         
+        if fig2 == True:
+            
+            fig_final = px.bar(novo_coladores, x="COLADOR", y="QUANTIDADE",
+                               color="STATUS", text_auto=True,
+                               barmode="group", height=400, width=500,
+                               title="COLADORES")
 
+            st.plotly_chart(fig_final)
+        
+        #transformando os número em data
+            df_data = df_dropidez.copy()
+            df_data["DATA"] = f"{df_data.iloc[0,0]:.0f}-{df_data.iloc[0,1]:.0f}-{df_data.iloc[0,2]:.0f}"
+            df_data_atual = df_data[df_data['COLADOR'] != "IDEZ"]
+            lista_data = list(df_data_atual['COLADOR'].unique())
+        
+            registros = st.checkbox("Verificar tabelas")
+        
+            if registros == True:
+                pesquisa = st.selectbox("Colador", lista_data)
 
+                df_tabela = df_data_atual[['DATA',"COLADOR",'QUANTIDADE','STATUS']]
+            
+                df_tabela_final = df_tabela[df_tabela['COLADOR'] == pesquisa]
+                df_apresentacao = df_tabela_final[['DATA','QUANTIDADE','STATUS']]
+                st.table(df_apresentacao)
